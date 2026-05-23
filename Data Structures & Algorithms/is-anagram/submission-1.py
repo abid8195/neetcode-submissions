28254086946167b97ffdf30a60ea1c3e8515dde0
abid8195 <116783337+abid8195@users.noreply.github.com>
2026-05-24 02:20:24 +1000

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s1 = {}
+        s2 = {}
+        for m in s:
+            if m in s1:
+                s1[m] += 1
+            else:
+                s1[m] = 1
+        for n in t:
+            if n in s2:
+                s2[n] += 1
+            else:
+                s2[n] = 1
+        if s1 == s2:
+            return True
+        else:
+            return False
+        
